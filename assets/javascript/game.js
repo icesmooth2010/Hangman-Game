a1ad132802a2ea8d71b1f4e23c0926148840d882
icesmooth2010 <32586621+icesmooth2010@users.noreply.github.com>
2018-02-01 22:07:02 -0700

    working onit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,7 +6,7 @@ prompt('Press any key to get started!');
 // list of different species of shark for hangman game.
 
 var islandWordsArr = ['antigua', 'beach', 'pineapple', 'shark', 
-'palmtree','ocean', 'sunblock', 'rum','waves', 'lifeguard', 'sun', 'breeze', 'exotic', 'bahamas', 'Lemon','spices', 'surfing', 'scuba', 'fish', 'beach ball']
+'palmtree','ocean', 'sunblock', 'rum','waves', 'lifeguard', 'sun', 'breeze', 'exotic', 'bahamas', 'lemon','spices', 'surfing', 'scuba', 'fish', 'beach ball']
 
 // using above array we choose a random word.
 var islandWords = islandWordsArr[Math.floor(Math.random() * islandWordsArr.length)];
@@ -17,9 +17,27 @@ var islandWords = islandWordsArr[Math.floor(Math.random() * islandWordsArr.lengt
 var s;
 var count = 0;
 var answerArray = [];
+// Start the game with a score of 0.
+var score = 0;
 
+var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+        'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+        't', 'u', 'v', 'w', 'x', 'y', 'z'];
+         // create alphabet ul
+  var buttons = function () {
+    myButtons = document.getElementById('buttons');
+    letters = document.createElement('ul');
 
-
+    for (var i = 0; i < alphabet.length; i++) {
+      letters.id = 'alphabet';
+      list = document.createElement('li');
+      list.id = 'letter';
+      list.innerHTML = alphabet[i];
+      check();
+      myButtons.appendChild(letters);
+      letters.appendChild(list);
+    }
+  }
 // filling the answer array with underscores as required
 // number of underscores correlates to the randomly selected word in the array
 
@@ -43,6 +61,8 @@ function letter() {
         answerArray[i] = letter;
       }
     }
+    // Captures the key press, converts it to lowercase, and saves it to a variable.
+    var letter = String.fromCharCode(event.which).toLowerCase();
     count++;
     document.getElementById("counter").innerHTML = "No of clicks: " + count;
     document.getElementById("answer").innerHTML = answerArray.join(" ");
@@ -58,3 +78,7 @@ console.log (islandWords.indexOf(event.key, 0))
 
 }
 startUp()
+ // Function that updates the score...
+ function updateScore() {
+  document.querySelector("#score").innerHTML = "Score: " + score;
+}
